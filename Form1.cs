a7591cbee21fabdeec43a4744c9df3de2353dd0f
渡邉 浩樹 <hi.watanabe@niigata.thk.co.jp>
2019-11-19 12:09:28 +0900

    Taskでコントロールを制御

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_by_Task
+{
+	public partial class Form1 : Form
+	{
+		private int Count = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			await Task.Run(() =>
+			{
+				Count++;
+				label1.Invoke((Action)(() => label1.Text = Count.ToString()));
+			});
+		}
+	}
+}
